f5232a57054bbe5cecc7f00ec141065b69175331
Laborator <vulpetheodor@gmail.com>
2021-09-29 18:07:32 +0300

    Misca

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/ArmTeleOP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/ArmTeleOP.java
@@ -1,8 +1,11 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.teleop;
 
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -14,16 +17,23 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 public class ArmTeleOP extends OpMode {
 
     DcMotorEx plateMotor, armMotor;
+    int platePosition, armPosition;
 
     @Override
     public void init() {
         plateMotor = hardwareMap.get(DcMotorEx.class, "plateMotor");
         armMotor = hardwareMap.get(DcMotorEx.class, "armMotor");
+
+        plateMotor.setZeroPowerBehavior(BRAKE);
+        armMotor.setZeroPowerBehavior(BRAKE);
+
+        plateMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     @Override
     public void loop() {
-        if (gamepad1.a) {
+        /**if (gamepad1.a) {
             plateMotor.setDirection(DcMotorSimple.Direction.FORWARD);
             plateMotor.setVelocity(25);
         } else if (gamepad1.b) {
@@ -39,6 +49,44 @@ public class ArmTeleOP extends OpMode {
             armMotor.setVelocity(25);
         } else
             armMotor.setVelocity(0);
+         */
+        if(gamepad1.a || gamepad1.b || gamepad1.right_bumper || gamepad1.left_bumper)
+        {
+            plateMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+            if (gamepad1.a)
+                plateMotor.setPower(0.1);
+            else if (gamepad1.b)
+                plateMotor.setPower(-0.1);
+
+            if (gamepad1.right_bumper)
+                armMotor.setPower(0.1);
+            else if (gamepad1.left_bumper)
+                armMotor.setPower(-0.1);
+
+            platePosition = plateMotor.getCurrentPosition();
+            armPosition = armMotor.getCurrentPosition();
+        }
+        else
+        {
+            //plateMotor.setPower(0);
+            //armMotor.setPower(0);
+
+            telemetry.addData("Pozitie din variabila:", platePosition);
+            telemetry.addData("Pozitie din motor:", plateMotor.getCurrentPosition());
+            telemetry.addData("Pozitie din variabila:", armPosition);
+            telemetry.addData("Pozitie din motor:", armMotor.getCurrentPosition());
+            telemetry.update();
+
+            plateMotor.setTargetPosition(platePosition);
+            plateMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            plateMotor.setPower(0.1);
+
+            armMotor.setTargetPosition(armPosition);
+            armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            armMotor.setPower(0.1);
+        }
     }
 
     /*
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/teleop/ArmTeleOPBackup.java
@@ -0,0 +1,84 @@
+package org.firstinspires.ftc.teamcode.drive.opmode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp(group = "drive")
+public class ArmTeleOPBackup extends OpMode {
+
+    DcMotorEx plateMotor, armMotor;
+    DcMotorEx rearLeftMotor, frontLeftMotor, rearRightMotor, frontRightMotor;
+    double drive, strafe, rotate;
+    double rearLeftPower, frontLeftPower, rearRightPower, frontRightPower;
+
+    @Override
+    public void init() {
+        plateMotor = hardwareMap.get(DcMotorEx.class, "plateMotor");
+        armMotor = hardwareMap.get(DcMotorEx.class, "armMotor");
+        frontLeftMotor = hardwareMap.get(DcMotorEx.class, "frontLeftMotor");
+        frontRightMotor = hardwareMap.get(DcMotorEx.class, "frontRightMotor");
+        rearLeftMotor = hardwareMap.get(DcMotorEx.class, "rearLeftMotor");
+        rearRightMotor = hardwareMap.get(DcMotorEx.class, "rearRightMotor");
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+
+        drive = gamepad1.right_stick_y;
+        strafe = -gamepad1.right_stick_x;
+        rotate = -gamepad1.right_trigger + gamepad1.left_trigger;
+
+        rearLeftPower = -strafe + drive + rotate;
+        frontLeftPower = strafe + drive + rotate;
+        rearRightPower = strafe + drive - rotate;
+        frontRightPower = -strafe + drive - rotate;
+
+        rearLeftPower = Range.clip(rearLeftPower, -1.0, 1.0);
+        rearRightPower = Range.clip(rearRightPower, -1.0, 1.0);
+        frontLeftPower = Range.clip(frontLeftPower, -1.0, 1.0);
+        frontRightPower = Range.clip(frontRightPower, -1.0, 1.0);
+
+        if(gamepad1.right_bumper){
+            rearLeftPower *= 0.3;
+            rearRightPower *= 0.3;
+            frontLeftPower *= 0.3;
+            frontRightPower *= 0.3;
+        }
+
+        rearLeftMotor.setPower(rearLeftPower);
+        rearRightMotor.setPower(rearRightPower);
+        frontLeftMotor.setPower(frontLeftPower);
+        frontRightMotor.setPower(frontRightPower);
+
+        if (gamepad2.a) {
+            plateMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            plateMotor.setVelocity(0.1);
+        } else if (gamepad2.b) {
+            plateMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            plateMotor.setVelocity(0.1);
+        } else {
+            plateMotor.setTargetPosition(plateMotor.getCurrentPosition());
+        }
+        if (gamepad2.right_bumper) {
+            armMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+            armMotor.setVelocity(0.1);
+        } else if (gamepad2.left_bumper) {
+            armMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+            armMotor.setVelocity(0.1);
+        } else {
+            plateMotor.setTargetPosition(plateMotor.getCurrentPosition());
+        }
+    }
+
+}
